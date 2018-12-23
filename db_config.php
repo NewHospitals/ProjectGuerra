d0b3bf038a3c2316b263b739d2d76a62023aab9a
shakye <shakyesam6@gmail.com>
2018-12-23 12:17:44 +0530

    Adding order summary and other minor changes

--- db_config.php
+++ db_config.php
@@ -1,14 +1,17 @@
 <?php
+
 define("HOST", "localhost");           
-define("USER", "guerra_user");              
-define("PASSWORD", "123456");  
-define("DATABASE", "projectguerra");     
+define("USER", "root");              
+define("PASSWORD", "");  
+define("DATABASE", "guerra");     
+ 
 define("CAN_REGISTER", "any");
 define("DEFAULT_ROLE", "member");
-define("SECURE", FALSE);   
-
+ 
+define("SECURE", FALSE);       
 $con = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
+
 if ( mysqli_connect_errno() ) {
 	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
+}      
 ?>
